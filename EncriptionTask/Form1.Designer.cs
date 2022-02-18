b2c54fa3b7e7ee7458f2024524fca0301e11f45c
Ali <noid379@gmail.com>
2022-02-18 20:44:26 +0400

    Final Commit

---
+++ EncriptionTask/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace EncriptionTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFile = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.TxtBoxFile = new System.Windows.Forms.TextBox();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.LabelProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnFile
+            // 
+            this.BtnFile.Location = new System.Drawing.Point(13, 13);
+            this.BtnFile.Name = "BtnFile";
+            this.BtnFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnFile.TabIndex = 0;
+            this.BtnFile.Text = "File";
+            this.BtnFile.UseVisualStyleBackColor = true;
+            this.BtnFile.Click += new System.EventHandler(this.FileBtn_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(260, 96);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(341, 96);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 2;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // TxtBoxFile
+            // 
+            this.TxtBoxFile.Location = new System.Drawing.Point(95, 15);
+            this.TxtBoxFile.Name = "TxtBoxFile";
+            this.TxtBoxFile.Size = new System.Drawing.Size(321, 20);
+            this.TxtBoxFile.TabIndex = 3;
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.Location = new System.Drawing.Point(95, 41);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.Size = new System.Drawing.Size(321, 20);
+            this.TextBoxPassword.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Password";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(95, 67);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(321, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // LabelProgress
+            // 
+            this.LabelProgress.AutoSize = true;
+            this.LabelProgress.Location = new System.Drawing.Point(26, 76);
+            this.LabelProgress.Name = "LabelProgress";
+            this.LabelProgress.Size = new System.Drawing.Size(21, 13);
+            this.LabelProgress.TabIndex = 7;
+            this.LabelProgress.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 126);
+            this.Controls.Add(this.LabelProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxPassword);
+            this.Controls.Add(this.TxtBoxFile);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.BtnFile);
+            this.MaximumSize = new System.Drawing.Size(440, 165);
+            this.MinimumSize = new System.Drawing.Size(440, 165);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFile;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.TextBox TxtBoxFile;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label LabelProgress;
+    }
+}
---
+++ EncriptionTask/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EncriptionTask
+{
+    public partial class Form1 : Form
+    {
+        // private Thread t;
+        private string _text;
+        private string _key;
+        private StringBuilder _proccesedText = new StringBuilder();
+        private CancellationTokenSource cts = new CancellationTokenSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgressPosition(int progress)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                progressBar1.BeginInvoke(new Action(() => progressBar1.Value = progress));
+                LabelProgress.BeginInvoke(new Action(() => LabelProgress.Text = progress.ToString()+@"%"));
+            }
+            else
+            {
+                progressBar1.Value=progress;
+                LabelProgress.Text = progress.ToString()+@"%";
+            }
+        }
+
+        private void EncDec(CancellationToken token)
+        {
+           
+            GetKey(); 
+            ReadFromFile();
+            _proccesedText.Clear();
+            for (int c = 0; c < _text.Length-1; c++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    ReverseOperation();
+                    return;
+                }
+                _proccesedText.Append((char)(_text[c] ^ (uint)_key[c % _key.Length]));
+                ProgressPosition(((c+1)*100/(_text.Length-1)));
+                Thread.Sleep(100);
+            }
+            WriteToFile();
+            ProgressPosition(0);
+            LabelProgress.BeginInvoke(new Action(() => LabelProgress.Text = @"Done"));
+        }
+
+        private void ReverseOperation()
+        {
+            File.WriteAllText(TxtBoxFile.Text, _text);
+            ProgressPosition(0);
+            LabelProgress.BeginInvoke(new Action(() => LabelProgress.Text = @"Cancelled"));
+        }
+
+        private void GetKey()
+        {
+            try
+            {
+                _key = TextBoxPassword.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReadFromFile()
+        {
+            try
+            {
+                _text = "";
+                foreach (string line in File.ReadLines(TxtBoxFile.Text))
+                {
+                    _text += line + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        }
+
+        private void WriteToFile()
+        {
+            File.WriteAllText(TxtBoxFile.Text, _proccesedText.ToString());
+        }
+
+        private void FileBtn_Click(object sender, EventArgs e)
+        {
+            var fd = new OpenFileDialog();
+            fd.Filter = @"Text|*.txt|All|*.*";
+            if (fd.ShowDialog() == DialogResult.OK) TxtBoxFile.Text = fd.FileName;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                EncDec(cts.Token);
+            });
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+             cts.Cancel();
+        }
+    }
+}
